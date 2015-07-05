3c6b2b80cf7bd5f3c76f89e4bff56ceb543f1b6b
Samantha <samantha.goldstein31@gmail.com>
2015-07-05 14:00:00 +0100

    comments

--- model/index.js
+++ model/index.js
@@ -72,20 +72,3 @@ if (!error && response.statusCode == 200) {
   });
 
 }
-
-// google.resultsPerPage =10
-// var nextCounter = 0
-//
-// google('node.js best practices', function (err, next, links){
-//   if (err) console.error(err)
-//
-//   for (var i = 0; i < links.length; ++i) {
-//     console.log(links[i].title + ' - ' + links[i].link) // link.href is an alias for link.link
-//     console.log(links[i].description + "\n")
-//   }
-//
-//   if (nextCounter < 4) {
-//     nextCounter += 1
-//     if (next) next()
-//   }
-// })
